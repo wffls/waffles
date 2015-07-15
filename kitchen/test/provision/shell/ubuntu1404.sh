1e068b80efd6db66af38aa7cda34f520cb850de8
Joe Topjian <joe@topjian.net>
2015-07-15 02:42:20 +0000

    config variable fixes

--- kitchen/test/provision/shell/ubuntu1404.sh
+++ kitchen/test/provision/shell/ubuntu1404.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 export WAFFLES_CONFIG_FILE=/root/.waffles/waffles.conf
-export SITE_DIR=/root/.waffles/kitchen/site
-export DEBUG=1
+export WAFFLES_SITE_DIR=/root/.waffles/kitchen/site
+export WAFFLES_DEBUG=1
 cd /root/.waffles
 bash waffles.sh -r ubuntu1404
