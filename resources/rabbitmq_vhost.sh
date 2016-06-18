03069eb88fe17e1729b2468eb4bccb38ed8fc5b8
Joe Topjian <joe@topjian.net>
2016-06-18 21:22:56 +0000

    re-adding rabbitmq resources

---
+++ resources/rabbitmq_vhost.sh
@@ -0,0 +1,51 @@
+# == Name
+#
+# rabbitmq.vhost
+#
+# === Description
+#
+# Manages RabbitMQ vhosts
+#
+# === Parameters
+#
+# * state: The state of the resource. Required. Default: present.
+# * vhost: The vhostname of the rabbitmq vhost. Required.
+#
+# === Example
+#
+# ```shell
+# rabbitmq.vhost --vhost openstack
+# ```
+#
+function rabbitmq.vhost {
+  # Declare the resource
+  waffles_resource="rabbitmq.vhost"
+
+  # Resource Options
+  local -A options
+  waffles.options.create_option state  "present"
+  waffles.options.create_option vhost  "__required__"
+  waffles.options.parse_options "$@"
+
+  # Process the resource
+  waffles.resource.process $waffles_resource "${options[vhost]}"
+}
+
+function rabbitmq.vhost.read {
+
+  rabbitmqctl -q list_vhosts 2>/dev/null | grep -q ${options[vhost]}
+  if [[ $? == 1 ]]; then
+    waffles_resource_current_state="absent"
+    return
+  fi
+
+  waffles_resource_current_state="present"
+}
+
+function rabbitmq.vhost.create {
+  exec.capture_error "rabbitmqctl add_vhost ${options[vhost]}"
+}
+
+function rabbitmq.vhost.delete {
+  exec.capture_error "rabbitmqctl delete_vhost ${options[vhost]}"
+}
