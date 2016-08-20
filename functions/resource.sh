fc7b762d517ad4c7ca3fec2ce00fc34071fd7a2d
Joe Topjian <joe@topjian.net>
2016-08-20 20:18:02 +0000

    Explicitly initialize waffles_total_changes to 0

--- functions/resource.sh
+++ functions/resource.sh
@@ -6,7 +6,7 @@ declare -g waffles_resource_current_state
 declare -g waffles_resource_changed
 
 # waffles_total_changes keeps track of how many resources have been changed.
-declare -g waffles_total_changes
+declare -g waffles_total_changes=0
 
 waffles.resource.check_dependencies() {
   while [[ $# -gt 0 ]]; do
@@ -77,17 +77,17 @@ waffles.resource.read() {
 waffles.resource.create() {
   "${_resource_type}.create"
   waffles_resource_changed="true"
-  waffles_total_changes=$(( ${waffles_total_changes:-0}+1 ))
+  waffles_total_changes=$(( $waffles_total_changes+1 ))
 }
 
 waffles.resource.update() {
   "${_resource_type}.update"
   waffles_resource_changed="true"
-  waffles_total_changes=$(( ${waffles_total_changes:-0}+1 ))
+  waffles_total_changes=$(( $waffles_total_changes+1 ))
 }
 
 waffles.resource.delete() {
   "${_resource_type}.delete"
   waffles_resource_changed="true"
-  waffles_total_changes=$(( ${waffles_total_changes:-0}+1 ))
+  waffles_total_changes=$(( $waffles_total_changes+1 ))
 }
