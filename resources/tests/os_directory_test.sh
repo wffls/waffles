3370d2fe87f992cdd47f315a06f1b61e82bb46f6
Joe Topjian <joe@topjian.net>
2016-08-21 03:53:41 +0000

    os.directory updates for set -eu

---
+++ resources/tests/os_directory_test.sh
@@ -0,0 +1,13 @@
+create() {
+  os.directory --name /opt/foo --mode 0755
+  os.directory --name /opt/a/b/c --parent true --mode 0755
+}
+
+update() {
+  os.directory --name /opt/foo --mode 0750
+}
+
+delete() {
+  os.directory --name /opt/foo --state absent
+  os.directory --name /opt/a/b/c --parent true --state absent
+}
