3acc4adabed9dc32454cec6fd18d47766fc04a8f
Joe Topjian <joe@topjian.net>
2016-08-21 03:01:56 +0000

    apt.key updated
    Better handling of remote_keyfile.
    Compatible with set -eu

---
+++ resources/tests/apt_key_test.sh
@@ -0,0 +1,13 @@
+create() {
+  apt.key --name rabbitmq --remote_keyfile https://www.rabbitmq.com/rabbitmq-release-signing-key.asc
+  apt.key --name percona --keyserver keys.gnupg.net --key CD2EFD2A
+}
+
+update() {
+  return
+}
+
+delete() {
+  apt.key --name rabbitmq --state absent --remote_keyfile https://www.rabbitmq.com/rabbitmq-release-signing-key.asc
+  apt.key --name percona --state absent --keyserver keys.gnupg.net --key CD2EFD2A
+}
