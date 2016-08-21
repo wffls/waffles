b9fe927779f24d1b1ce6fdd5832b94ba31b124c8
Joe Topjian <joe@topjian.net>
2016-08-21 04:01:25 +0000

    apt.source updates for set -eu

---
+++ resources/tests/apt_source_test.sh
@@ -0,0 +1,11 @@
+create() {
+  apt.source --name rabbitmq --uri http://www.rabbitmq.com/debian/ --distribution testing --component main --include_src false
+}
+
+update() {
+  apt.source --name rabbitmq --uri http://www.rabbitmq.com/debian/ --distribution testing --component main --include_src true
+}
+
+delete() {
+  apt.source --name rabbitmq --state absent --uri http://www.rabbitmq.com/debian/ --distribution testing --component main --include_src false
+}
